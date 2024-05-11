d3d323224817ee801d4329ce2a6692eebf66cb39
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2024-05-11 21:20:43 +0800

    configurable mining level

--- common/src/main/java/net/anawesomguy/breakingbedrock/BedrockBlock.java
+++ common/src/main/java/net/anawesomguy/breakingbedrock/BedrockBlock.java
@@ -14,8 +14,8 @@ public class BedrockBlock extends Block {
 
     @Override @SuppressWarnings("deprecation")
     public float getDestroyProgress(BlockState state, Player player, BlockGetter level, BlockPos pos) {
-        return player.getInventory().getSelected().getItem() instanceof TieredItem item && item.getTier().getLevel() > 3 ?
-            player.getDestroySpeed(state) / state.getDestroySpeed(level, pos) / 100F :
-            0F;
+        return player.getInventory().getSelected().getItem() instanceof TieredItem item && item.getTier().getLevel() >= BreakingBedrock.BEDROCK_MINING_TIER ?
+               player.getDestroySpeed(state) / state.getDestroySpeed(level, pos) / 100F :
+               0F;
     }
 }
--- common/src/main/java/net/anawesomguy/breakingbedrock/BreakingBedrock.java
+++ common/src/main/java/net/anawesomguy/breakingbedrock/BreakingBedrock.java
@@ -17,42 +17,54 @@ public final class BreakingBedrock {
 
     public static final float BEDROCK_DESTROY_TIME;
     public static final float BEDROCK_EXPLOSION_RESIST;
+    public static final int BEDROCK_MINING_TIER;
 
     static {
         Properties properties = new Properties();
         float destroyTime, explosionResist;
+        int miningLevel;
         File configFile = configDir().resolve(BreakingBedrock.MOD_ID + ".properties").toFile();
         try {
             Properties temp = new Properties();
             temp.load(new FileInputStream(configFile));
             String time = temp.getProperty("bedrockDestroyTime"),
-                resist = temp.getProperty("bedrockExplosionResist");
+                   resist = temp.getProperty("bedrockExplosionResist"),
+                   level = temp.getProperty("bedrockMiningTier");
             if (time == null || !((destroyTime = Float.parseFloat(time)) > -1) || destroyTime == Float.POSITIVE_INFINITY) {
                 properties.setProperty("bedrockDestroyTime", "71");
-                destroyTime = 71;
+                destroyTime = 71F;
                 LOGGER.debug("Correcting invalid config value {}!", time);
             } else properties.setProperty("bedrockDestroyTime", time);
             if (resist == null || !((explosionResist = Float.parseFloat(resist)) > 0) || explosionResist == Float.POSITIVE_INFINITY) {
                 properties.setProperty("bedrockExplosionResist", "3600000");
-                explosionResist = 3600000;
+                explosionResist = 3600000F;
                 LOGGER.debug("Correcting invalid config value {}!", resist);
             } else properties.setProperty("bedrockExplosionResist", resist);
+            if (level == null || !((miningLevel = Integer.parseInt(level)) > 0)) {
+                properties.setProperty("bedrockMiningLevel", "4");
+                miningLevel = 4;
+                LOGGER.debug("Correcting invalid config value {}!", level);
+            } else properties.setProperty("bedrockMiningLevel", resist);
         } catch (IOException | IllegalArgumentException e) {
             LOGGER.info("Could not read config file (likely corrupted or missing)! Attempting to (re)create it.");
             properties.setProperty("bedrockDestroyTime", "71");
             properties.setProperty("bedrockExplosionResist", "3600000");
-            explosionResist = 71;
-            destroyTime = 3600000;
+            properties.setProperty("bedrockMiningLevel", "4");
+            explosionResist = 71F;
+            destroyTime = 3600000F;
+            miningLevel = 4;
         }
 
         BEDROCK_EXPLOSION_RESIST = explosionResist;
         BEDROCK_DESTROY_TIME = destroyTime;
+        BEDROCK_MINING_TIER = miningLevel;
 
         try {
             properties.store(new FileOutputStream(configFile),
                   """
                   bedrockDestroyTime: The destroy time for bedrock. (obsidian is 50, stone is 1.5, -1 means indestructible)
                   bedrockExplosionResist: The explosion resistance for bedrock. (stone is 6, glass is 0.3)
+                  bedrockExplosionResist: The mining level required to mine bedrock. (netherite is 4)
                  """);
         } catch (IOException e) {
             LOGGER.error("Unable to create/modify config file!", e);
